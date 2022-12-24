f189670de5ad86e90862f81c43491c9a681502f7
IAMTHEMACHINE33 <iamthemachine33@gmail.com>
2022-12-24 16:19:45 +0545

    user authentication

---
+++ auth/auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+
+module.exports.userGuard=(req,res,next)=>{
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const data = jwt.verify(token, "token");
+        console.log(data);
+        User.findOne({_id:data.userId})
+        .then((user_data)=>{
+            req.user = user_data;
+            next();
+        })
+        .catch((e)=>{
+            res.json({success:false,msg:"Invalid token"})
+        })
+    } catch (e) {
+        res.json({success:false,msg:e})
+    }
+}
